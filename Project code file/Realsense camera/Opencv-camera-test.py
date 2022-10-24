a84d6df5d0a4d57ea16b4308d28ae850c706fcb7
Muhammad Farhan Oktavian <farhanoktvn@gmail.com>
2022-10-24 16:26:56 +0100

    Update Opencv-camera-test.py

--- Project code file/Realsense camera/Opencv-camera-test.py
+++ Project code file/Realsense camera/Opencv-camera-test.py
@@ -2,6 +2,39 @@ import pyrealsense2 as rs
 import numpy as np
 import cv2
 
+def aruco_display(corners, ids, rejected, image):
+	if len(corners) > 0:
+		# flatten the ArUco IDs list
+		ids = ids.flatten()
+		# loop over the detected ArUCo corners
+		for (markerCorner, markerID) in zip(corners, ids):
+			# extract the marker corners (which are always returned in
+			# top-left, top-right, bottom-right, and bottom-left order)
+			corners = markerCorner.reshape((4, 2))
+			(topLeft, topRight, bottomRight, bottomLeft) = corners
+			# convert each of the (x, y)-coordinate pairs to integers
+			topRight = (int(topRight[0]), int(topRight[1]))
+			bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+			bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+			topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+			cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
+			cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
+			cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
+			cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+			# compute and draw the center (x, y)-coordinates of the ArUco
+			# marker
+			cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+			cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+			cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
+			# draw the ArUco marker ID on the image
+			cv2.putText(image, str(markerID),(topLeft[0], topLeft[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+				0.5, (0, 255, 0), 2)
+			print("[Inference] ArUco marker ID: {}".format(markerID))
+			# show the output image
+	return image
+
+
 if __name__ == "__main__":
     # Configure depth and color streams
     pipeline = rs.pipeline()
@@ -12,7 +45,7 @@ if __name__ == "__main__":
     pipeline.start(config)
 
     # set aruco dictionary and parameters
-    aruco_dict = cv2.aruco.Dictionary_get(ARUCO_DICT["DICT_4X4_50"])
+    aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
     aruco_params = cv2.aruco.DetectorParameters_create()
 
     try:
@@ -43,8 +76,7 @@ if __name__ == "__main__":
 
             corners, ids, rejected = cv2.aruco.detectMarkers(color_image, aruco_dict, parameters=aruco_params)
             detected_markers = aruco_display(corners, ids, rejected, color_image)
-            colorized_streams.append(detected_markers)
-
+            cv2.imshow('RealSense', detected_markers)
 
 
             key = cv2.waitKey(100)
