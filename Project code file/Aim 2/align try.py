3f2e3a93b4c7e585b484f7fa22e58fa04e7cad37
JY522IC <jy522@ic.ac.uk>
2022-11-10 12:44:10 +0000

    adding functions to track blob in 3d space

---
+++ Project code file/Aim 2/align try.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2022/11/10 12:12
+# @Author  : Yiyang
+# @File    : align try.py
+# @Contact: jy522@ic.ac.uk
---
+++ Project code file/Aim 2/camera.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2022/11/4 10:41
+# @Author  : Yiyang
+# @File    : camera.py
+# @Contact: jy522@ic.ac.uk
