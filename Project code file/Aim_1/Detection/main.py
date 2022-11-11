3cd8e2245687346acaa8d5544681ab23189d2702
wenkang li <sc19wl@leeds.ac.uk>
2022-11-11 12:34:53 +0000

    remove space in folder names

--- Project code file/Aim_1/Detection/main.py
+++ Project code file/Aim_1/Detection/main.py
@@ -9,18 +9,18 @@ from markers.blob import BlobMarker
 if __name__ == "__main__":
 
     # Instantiate camera
-    # cam = camera.RealsenseCamera() # intel realsense
-    cam = camera.WebcamCamera() # webcam
+    cam = camera.RealsenseCamera() # intel realsense
+    # cam = camera.WebcamCamera() # webcam
 
     # Load camera calibration
-    # cam_cal = np.load("calibration/calibration_realsense.npz")
-    cam_cal = np.load('calibration/calibration_webcam.npz')
+    cam_cal = np.load("D:\works\Powerpoint & PDF\Postgraduate_study\Group_project\group_project\Project code file\Aim 1\Detection\calibration\calibration_realsense.npz")
+    # cam_cal = np.load('D:\works\Powerpoint & PDF\Postgraduate_study\Group_project\group_project\Project code file\Aim 1\Detection\calibration\calibration_webcam.npz')
     camera_matrix = cam_cal['camera_matrix']
     dist_coef = cam_cal['dist_coef']
 
     # Instantiate marker detector
-    # mark = ArucoMarker(camera_matrix, dist_coef)
-    mark = BlobMarker(camera_matrix, dist_coef)
+    mark = ArucoMarker(camera_matrix, dist_coef)
+    # mark = BlobMarker(camera_matrix, dist_coef)
 
     # Loop until escaped
     try:
@@ -36,8 +36,8 @@ if __name__ == "__main__":
             # frame = cv2.filter2D(frame, -1, log_kernel)
             
             # Detect markers
-            image = mark.detect_and_display_boundary(frame)
-            # image = mark.detect_and_display_pose(frame)
+            # image = mark.detect_and_display_boundary(frame)
+            image = mark.detect_and_display_pose(frame)
 
             # Show image
             cv2.imshow('Detect Markers', image)
